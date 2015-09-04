1d55d93f85a7671c05d2fdad13ca49e03a13ed7b
Nick Desaulniers <ndesaulniers@mozilla.com>
2015-09-04 16:50:36 -0700

    add animation easing functions Fixes #75

--- src/vr-animation.js
+++ src/vr-animation.js
@@ -1,6 +1,5 @@
 var VRUtils = require('./vr-utils');
 var VRNode = require('./core/vr-node');
-
 var TWEEN = require('tween.js');
 
 module.exports = document.registerElement(
@@ -9,13 +8,29 @@ module.exports = document.registerElement(
     VRNode.prototype, {
       createdCallback: {
         value: function() {
+          this.attribute = this.getAttribute('attribute');
           this.delay = parseFloat(this.getAttribute('delay')) || 0;
           this.duration = parseFloat(this.getAttribute('duration')) || 1000;
-          this.loop = this.hasAttribute('loop');
-          this.attribute = this.getAttribute('attribute');
+          this.easing = this.setupEasing();
           this.to = VRUtils.parseAttributeString(this.attribute, this.getAttribute('to'));
           this.load();
         }
+      },
+
+      setupEasing: {
+        value: function() {
+          // See list of supported easing functions:
+          // https://github.com/tweenjs/tween.js/blob/master/docs/user_guide.md#available-easing-functions-tweeneasing
+          var easingAttr = this.getAttribute('easing');
+          var easingDirection = this.getAttribute('direction');
+
+          if (easingAttr && easingAttr in TWEEN.Easing &&
+              easingDirection in TWEEN.Easing[easingAttr]) {
+            return TWEEN.Easing[easingAttr][easingDirection];
+          } else {
+            return TWEEN.Easing.Linear.None;
+          }
+        },
       },
 
       add: {
@@ -24,6 +39,7 @@ module.exports = document.registerElement(
           var from = obj.getAttribute(attribute);
           new TWEEN.Tween(from)
             .to(this.to, this.duration)
+            .easing(this.easing)
             .delay(this.delay)
             .onUpdate(function () {
               obj.setAttribute(attribute, this);
