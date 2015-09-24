b1dc176c24c7d6cba0450097ac1c9851fd442877
caseyyee <caseyyee.ca@gmail.com>
2015-09-24 16:01:51 -0700

    hmd orientation and position tracking, walk and fly controls

--- src/vr-controls.js
+++ src/vr-controls.js
@@ -21,9 +21,9 @@ module.exports = document.registerElement(
             this.keys = {};
             this.mouseDown = false;
 
-            this.acceleration = 65;
+            this.acceleration = 6;
+            this.easing = 20;
             this.velocity = new THREE.Vector3();
-
             this.pitchObject = new THREE.Object3D();
             this.yawObject = new THREE.Object3D();
             this.yawObject.position.y = 10;
@@ -33,6 +33,7 @@ module.exports = document.registerElement(
             this.setAttribute('mouselook', true);
 
             this.attachMouseKeyboardListeners();
+            this.attachVrControls();
             this.load();
           }
         },
@@ -41,28 +42,36 @@ module.exports = document.registerElement(
           value: function () {
             this.locomotion = this.getAttribute('locomotion', false);
             this.mouseLook = this.getAttribute('mouselook', false);
+            this.controlType = this.getAttribute('type', 'walk');
           }
         },
 
         update: {
           value: function () {
             var velocity = this.velocity;
+            var easing = this.easing;
+            var acceleration = this.acceleration;
             var pitchObject = this.pitchObject;
             var yawObject = this.yawObject;
             var time = window.performance.now();
             var delta = (time - this.prevTime) / 1000;
             var keys = this.keys;
-            var acceleration = this.acceleration;
             this.prevTime = time;
 
-            velocity.x -= velocity.x * 10.0 * delta;
-            velocity.z -= velocity.z * 10.0 * delta;
+            velocity.x -= velocity.x * easing * delta;
+            velocity.z -= velocity.z * easing * delta;
 
             var position = this.getAttribute('position', {x: 0, y: 0, z: 0});
             var rotation = this.getAttribute('rotation', {x: 0, y: 0, z: 0});
             var rotZ = rotation.z;
 
             if (this.locomotion) {
+              if (keys[81]) { // rotate left
+                yawObject.rotation.y += 0.05;
+              }
+              if (keys[69]) { // rotate right
+                yawObject.rotation.y -= 0.05;
+              }
               if (keys[65]) { // Left
                 velocity.x -= acceleration * delta;
               }
@@ -78,12 +87,73 @@ module.exports = document.registerElement(
             }
 
             if (keys[90]) { // Z
-              this.reset();
-              // scene.resetSensor();
-
-              position = this.getAttribute('position', {x: 0, y: 0, z: 0});
-              rotation = this.getAttribute('rotation', {x: 0, y: 0, z: 0});
+              this.vrControls.resetSensor();
             }
+
+            // forward, lateral and vertical movements.
+            var fwdX = 0;
+            var fwdZ = 0;
+            var latX = 0;
+            var latZ = 0;
+            var vertY = 0;
+
+            // hmd state
+            var hmdState = this.vrControls.state;
+
+            if (hmdState && hmdState.orientation !== null) {
+              rotation = this.vrControls.rotation;
+              this.hmdOrientation = true;
+            } else {
+              rotation = this.object3D.quaternion;
+            }
+
+            // apply rotation to forward and lateral vectors.
+            var fwd = new THREE.Vector3(0, 0, -1).applyQuaternion(rotation);
+            var lat = new THREE.Vector3(1, 0, 0).applyQuaternion(rotation);
+
+            switch (this.controlType) {
+              case 'walk':
+                // we want to rotate around Y, so we reset to 0 value.
+                fwd.y = 0;
+
+                // calculate corodinate for forward movement
+                var fwdRot = Math.atan2(fwd.x, -fwd.z);
+                fwdX = Math.sin(fwdRot);
+                fwdZ = -Math.cos(fwdRot);
+
+                // calculate coordinates for lateral movement
+                var latRot = Math.atan2(lat.x, -lat.z);
+                latX = Math.sin(latRot);
+                latZ = -Math.cos(latRot);
+
+                // apply velocity
+                fwdX *= -velocity.z;
+                fwdZ *= -velocity.z;
+                latX *= velocity.x;
+                latZ *= velocity.x;
+
+                break;
+              case 'fly':
+                // apply velocity
+                fwd.multiplyScalar(-velocity.z);
+                lat.multiplyScalar(velocity.x);
+
+                fwdX = fwd.x;
+                latX = lat.x;
+                vertY = fwd.y + lat.y;
+                fwdZ = fwd.z + lat.z;
+
+                break;
+              default:
+                console.warn('[vr-controls] Valid movement type is "fly" or "walk"');
+                break;
+            }
+
+            position = {
+              x: position.x + fwdX + latX,
+              y: position.y + vertY,
+              z: position.z + fwdZ + latZ
+            };
 
             this.setAttribute('rotation', {
               x: THREE.Math.radToDeg(pitchObject.rotation.x),
@@ -91,12 +161,9 @@ module.exports = document.registerElement(
               z: rotZ
             });
 
-            var movementVector = this.getMovementVector(delta);
-            this.setAttribute('position', {
-              x: position.x + movementVector.x,
-              y: position.y,
-              z: position.z + movementVector.z
-            });
+            this.setAttribute('position', position);
+
+            this.vrControls.update();
           }
         },
 
@@ -115,6 +182,12 @@ module.exports = document.registerElement(
           }
         },
 
+        attachVrControls: {
+          value: function () {
+            this.vrControls = new THREE.VRControls(this.object3D);
+          }
+        },
+
         onMouseMove: {
           value: function (event) {
             var pitchObject = this.pitchObject;
@@ -125,6 +198,11 @@ module.exports = document.registerElement(
 
             var movementX = event.movementX || event.mozMovementX || event.webkitMovementX || 0;
             var movementY = event.movementY || event.mozMovementY || event.webkitMovementY || 0;
+
+            // we want to lock-out any pitch when VR orientation is present.
+            if (this.hmdOrientation) {
+              movementY = 0;
+            }
 
             yawObject.rotation.y -= movementX * 0.002;
             pitchObject.rotation.x -= movementY * 0.002;
@@ -155,18 +233,6 @@ module.exports = document.registerElement(
         onKeyUp: {
           value: function (event) {
             this.keys[event.keyCode] = false;
-          }
-        },
-
-        getMovementVector: {
-          value: function (delta) {
-            var velocity = this.velocity;
-            var direction = new THREE.Vector3(velocity.x * delta, 0, velocity.z * delta);
-            var rotation = new THREE.Euler(0, 0, 0, 'YXZ');
-            var pitchObject = this.pitchObject;
-            var yawObject = this.yawObject;
-            rotation.set(pitchObject.rotation.x, yawObject.rotation.y, 0);
-            return direction.applyEuler(rotation);
           }
         }
       })
