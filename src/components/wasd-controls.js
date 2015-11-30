452e63e3ced4178d7767b6d8d6c2040a4f4a0eda
Diego Marcos <diego.marcos@gmail.com>
2015-11-29 22:03:00 -0700

    Makes wasd controls more configurable by letting the user specify the axis that WS and AD keys will modify.

--- src/components/wasd-controls.js
+++ src/components/wasd-controls.js
@@ -1,13 +1,36 @@
 var registerComponent = require('../core/register-component').registerComponent;
 var THREE = require('../../lib/three');
 
+/**
+ * WASD component.
+ *
+ * Control your entities with the WASD keys.
+ *
+ * @namespace wasd-controls
+ * @param {number} [easing=20] - How fast the movement decelerates. If you hold the
+ * keys the entity moves and if you release it will stop. Easing simulates friction.
+ * @param {string} [acceleration=65] - Determines the acceleration given
+ * to the entity when pressing the keys
+ * @param {number} [enabled=true] - To completely enable or disable the controls
+ * @param {number} [fly=false] - Determines if the direction of the movement sticks
+ * to the plane where the entity started off or if there are 6 degrees of
+ * freedom as a diver underwater or a plane flying.
+ * @param {string} [wsAxis='z'] - The axis that the W and S keys operate on
+ * @param {string} [adAxis='z'] - The axis that the A and D keys operate on
+ * @param {string} [wsInverted=false] - WS Axis is inverted
+ * @param {string} [adInverted=false] - AD Axis is inverted
+ */
 module.exports.Component = registerComponent('wasd-controls', {
   defaults: {
     value: {
       easing: 20,
       acceleration: 65,
       enabled: true,
-      fly: false
+      fly: false,
+      wsAxis: 'z',
+      adAxis: 'x',
+      wsInverted: false,
+      adInverted: false
     }
   },
 
@@ -30,7 +53,7 @@ module.exports.Component = registerComponent('wasd-controls', {
   },
 
   update: {
-    value: function () {
+    value: function (previousData) {
       var data = this.data;
       var acceleration = data.acceleration;
       var easing = data.easing;
@@ -39,19 +62,30 @@ module.exports.Component = registerComponent('wasd-controls', {
       var delta = (time - this.prevTime) / 1000;
       var keys = this.keys;
       var movementVector;
+      var adAxis = data.adAxis;
+      var wsAxis = data.wsAxis;
+      var adSign = data.adInverted ? -1 : 1;
+      var wsSign = data.wsInverted ? -1 : 1;
       var el = this.el;
       this.prevTime = time;
 
-      velocity.x -= velocity.x * easing * delta;
-      velocity.z -= velocity.z * easing * delta;
+      // If data has changed we reset the velocity
+      if (previousData || delta > 0.2) {
+        velocity[adAxis] = 0;
+        velocity[wsAxis] = 0;
+        return;
+      }
+
+      velocity[adAxis] -= velocity[adAxis] * easing * delta;
+      velocity[wsAxis] -= velocity[wsAxis] * easing * delta;
 
       var position = el.getComputedAttribute('position');
 
       if (this.data.enabled) {
-        if (keys[65]) { velocity.x -= acceleration * delta; } // Left
-        if (keys[68]) { velocity.x += acceleration * delta; } // Right
-        if (keys[87]) { velocity.z -= acceleration * delta; } // Up
-        if (keys[83]) { velocity.z += acceleration * delta; } // Down
+        if (keys[65]) { velocity[adAxis] -= adSign * acceleration * delta; } // Left
+        if (keys[68]) { velocity[adAxis] += adSign * acceleration * delta; } // Right
+        if (keys[87]) { velocity[wsAxis] -= wsSign * acceleration * delta; } // Up
+        if (keys[83]) { velocity[wsAxis] += wsSign * acceleration * delta; } // Down
       }
 
       movementVector = this.getMovementVector(delta);
@@ -94,7 +128,8 @@ module.exports.Component = registerComponent('wasd-controls', {
       return function (delta) {
         var velocity = this.velocity;
         var elRotation = this.el.getAttribute('rotation');
-        direction.set(velocity.x * delta, 0, velocity.z * delta);
+        direction.copy(velocity);
+        direction.multiplyScalar(delta);
         if (!elRotation) { return direction; }
         if (!this.data.fly) { elRotation.x = 0; }
         rotation.set(THREE.Math.degToRad(elRotation.x),
